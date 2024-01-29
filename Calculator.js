22453463fad87b7e2bb01bcabf374cd66fc5f7cb
Elhen15 <elhen15@Gmail.com>
2024-01-29 13:14:50 +0200

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -7,6 +7,7 @@ module.exports = {
         }, 0);
         console.log("aaa")
         console.log("aaa")
+        console.log("PULL REQUEST TEST")
         return sum;
     },
     substract: function() {
