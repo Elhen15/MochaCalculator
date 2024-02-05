1def50faa0ff5bf624a841048dec4d1d9fb48e26
Elhen15 <elhen15@Gmail.com>
2024-02-05 17:35:26 +0200

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,25 +1,25 @@
-var _ = require("underscore");
-
-module.exports = {
-    add: function() {
-        var sum = _.reduce(arguments, function(memo, numb) {
-            return memo + numb;
-        }, 0);
-        console.log("aaa")
-        console.log("aaa")
-        return sum;
-    },
-    substract: function() {
-
-        var substraction = 0;
-
-        return substraction + arguments[0];
-    },
-    multiply: function() {
-         var multiplication = _.reduce(arguments, function(memo, numb) {
-             if (isNaN(numb)) { throw "not a number"}
-            return memo * numb;
-        }, 1);
-        return multiplication;
-    }
+function add(...args) {
+    let sum = args.reduce((memo, numb) => memo + numb, 0);
+    console.log("aaa");
+    console.log("aaa");
+    return sum;
 }
+
+function subtract(...args) {
+    if (args.length === 0) return 0; // Handle case with no arguments
+    let subtraction = args[0];
+    for (let i = 1; i < args.length; i++) {
+        subtraction -= args[i];
+    }
+    return subtraction;
+}
+
+function multiply(...args) {
+    let multiplication = args.reduce((memo, numb) => {
+        if (isNaN(numb)) { throw new Error("not a number"); }
+        return memo * numb;
+    }, 1);
+    return multiplication;
+}
+
+module.exports = { add, subtract, multiply };
