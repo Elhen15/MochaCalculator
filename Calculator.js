e14028a7471b7c64dbd31b5f2c5bb44cc335ee80
Elhen15 <elhen15@Gmail.com>
2024-09-16 15:06:22 +0300

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,12 +1,11 @@
 function add(...args) {
     let sum = args.reduce((memo, numb) => memo + numb, 0);
     console.log("aaa");
-    console.log("aaa");
-    console.log("vbv");
     return sum;
 }
 
 function subtract(...args) {
+    console.log("aaa");
     if (args.length === 0) return 0; // Handle case with no arguments
     let subtraction = args[0];
     for (let i = 1; i < args.length; i++) {
@@ -16,6 +15,7 @@ function subtract(...args) {
 }
 
 function multiply(...args) {
+    console.log("aaa");
     let multiplication = args.reduce((memo, numb) => {
         if (isNaN(numb)) { throw new Error("not a number"); }
         return memo * numb;
